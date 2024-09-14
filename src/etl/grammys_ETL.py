8463b92ba95fe23e5c7460ffd552fc70f355aa70
JuanRuizING <juan_andres.ruiz@uao.edu.co>
2024-09-14 02:40:34 -0500

    the transform files and EDAs transform are ready

--- src/etl/grammys_ETL.py
+++ src/etl/grammys_ETL.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import os
 import logging
 from src.database.database import creating_engine
 
-def extract_grammys():
+def extract_db():
     #Read the df
     try:
         #Read the environment variables
@@ -16,3 +15,11 @@ def extract_grammys():
     except Exception as e:
         print(f'Error: {e}')
     return df_grammy.to_json(orient='records')
+
+def transform_db(json_data):
+    print("data coming from extract:", json_data)
+    print("data type is: ", type(json_data))
+    data = pd.json_normalize(data=json_data)
+    #function transform
+    #return json_data
+    pass
